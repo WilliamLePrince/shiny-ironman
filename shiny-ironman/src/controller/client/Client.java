ca3dc19b06611cf91202ab3ecf31458b21c71ae6
Romain <ceccarelli.romain@gmail.com>
2013-02-18 17:38:40 +0100

    J'ai modifié la classe ClientWindow avec un modèle Observer/Observable. Les problèmes de codes sont en commentaires.

---
+++ shiny-ironman/src/controller/client/Client.java
@@ -0,0 +1,22 @@
+package controller.client;
+
+import view.client.ClientWindow;
+
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ClientWindow client = new ClientWindow();
+		/*client.addObserver(new Observer()
+		{
+			public void update(client.choice); 
+		});*/
+		
+		
+	}
+
+}
