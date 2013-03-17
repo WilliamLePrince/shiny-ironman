834dff8cd14c71971a22034f573ad529521b7d44
jb <jb@N73JQ-JB>
2013-03-17 18:38:55 +0100

    Ajout de classes pour le serveur

---
+++ shiny-ironman/src/controler/server/Server.java
@@ -0,0 +1,22 @@
+package controler.server;
+
+import java.net.Socket;
+
+import model.school.School;
+import model.users.Userbase;
+
+public class Server {
+	protected static School s;
+	protected static Userbase u;
+	
+	protected static int port = 6789;
+	protected static String file = "data";
+	protected static Socket socketClient;
+	
+	
+	public static void main( String[] argv ){
+		
+			
+		
+	}
+}
---
+++ shiny-ironman/src/controler/server/ServerThreads.java
@@ -0,0 +1,9 @@
+package controler.server;
+
+public class ServerThreads implements Runnable{
+
+	@Override
+	public void run() {		
+	}
+	
+}
